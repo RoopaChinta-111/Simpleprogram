8d81ca89cb025f96bda2a6a7d71a2d0366a5079e
RoopaChinta-111 <roopa.c1409@gmail.com>
2024-10-22 12:12:58 +0530

    Update HelloWorld.java

--- src/example/HelloWorld.java
+++ src/example/HelloWorld.java
@@ -5,6 +5,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
        System.out.println("Welcome program");
+       System.out.println("Commit the changes");
 	}
 
 }
